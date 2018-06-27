e3cd47c3f6ef904f58c258c819a72ae919d4b6ee
Admin0705 <user@dunn.cloud>
2018-06-27 02:18:40 -0400

    update

--- scripts/docker-no/upgrade2.sh
+++ scripts/docker-no/upgrade2.sh
@@ -1,4 +1,4 @@
 cd ~
 sudo rm -r /opt/plexg* 2>/dev/nu*
-git clone https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server.git /opt/plexguide
+sudo git clone https://github.com/Admin9705/PlexGuide.com-The-Awesome-Plex-Server.git --branch Version-6 /opt/plexguide
 bash /opt/plexg*/ins*
